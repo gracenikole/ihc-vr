b0a8070537902599efa8cdd0e54c71a177698262
Josedlz <jose.delama@utec.edu.pe>
2023-06-24 13:16:09 -0500

    Place a tree with cursor

--- Assets/Scripts/GrowingTreeManager.cs
+++ Assets/Scripts/GrowingTreeManager.cs
@@ -9,21 +9,27 @@ public class GrowingTreeManager : MonoBehaviour
     public float growRate;
     private float scale = 0.0f;
     public Color[] colorVariations;
+    private bool shouldGrow = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        maxSize = Random.Range(0.0f, 2.0f); 
-        growRate = Random.Range(0.2f, 0.8f);    
+        maxSize = Random.Range(100.0f, 200.0f); 
+        growRate = Random.Range(2.0f, 8.0f);    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (scale < maxSize)
+        if (shouldGrow && scale < maxSize)
         {
             transform.localScale = Vector3.one * scale;
             scale += growRate * Time.deltaTime;
         }
+    }
+
+    public void ToggleGrow()
+    {
+        shouldGrow = !shouldGrow;
     }
 }
