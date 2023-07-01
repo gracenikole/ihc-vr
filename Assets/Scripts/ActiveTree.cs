b356eb17e18155d51708a6bb5b4a439da902e075
Renato Rodriguez Llanos <renato.rodriguez.l@utec.edu.pe>
2023-07-01 17:05:39 -0500

    feat(2rf): set habit canvas implemented

--- Assets/Scripts/ActiveTree.cs
+++ Assets/Scripts/ActiveTree.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class ActiveTree : MonoBehaviour
 {
     public GameObject plantedTree;
+    public GameObject canvas;
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.CompareTag("UnplantedTree"))
         {
             plantedTree.SetActive(true);
+            canvas.SetActive(true);
         }
     }
 }
