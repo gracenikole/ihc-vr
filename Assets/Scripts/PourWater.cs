7ae9438e40634fa53145107668616e998291a6d0
Josedlz <jose.delama@utec.edu.pe>
2023-07-07 22:58:57 -0500

    water can

---
+++ Assets/Scripts/PourWater.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourWater : MonoBehaviour
+{
+    public float thresholdAngle = 45f; // The angle of tilting to start pouring
+    public ParticleSystem waterParticles; // The Particle System
+
+    void Update()
+    {
+        // Check if the watering can is tilted enough
+        if (Vector3.Angle(Vector3.up, transform.up) > thresholdAngle)
+        {
+            // If it is, start the Particle System
+            if (!waterParticles.isPlaying)
+            {
+                waterParticles.Play();
+            }
+        }
+        else
+        {
+            // If it's not, stop the Particle System
+            if (waterParticles.isPlaying)
+            {
+                waterParticles.Stop();
+            }
+        }
+    }
+}
