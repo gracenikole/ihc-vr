00cc61ac6fe65ca71944834c662d3bec0584ce1a
gracenikole <gracenikole@gmail.com>
2023-07-08 05:05:58 -0500

    test tree

--- Assets/Scripts/GenerateNewTree.cs
+++ Assets/Scripts/GenerateNewTree.cs
@@ -7,11 +7,15 @@ public class GenerateNewTree : MonoBehaviour
    public GameObject prefabToGenerate;
 
     public void Generate() {
-        float customPositionX = 0.00999999F;
-        float customPositionY = 1.57F;
-        float customPositionZ = -0.201F;
-        Vector3 customPosition = new Vector3(customPositionX, customPositionY, customPositionZ);
+        Vector3 position = transform.position;
+        Instantiate(prefabToGenerate, position, Quaternion.identity);
+        // gameObject.SetActive(false);
 
-        Instantiate(prefabToGenerate, customPosition, transform.rotation);
+        // float customPositionX = 0.00999999F;
+        // float customPositionY = 1.57F;
+        // float customPositionZ = -0.201F;
+        // Vector3 customPosition = new Vector3(customPositionX, customPositionY, customPositionZ);
+
+        // Instantiate(prefabToGenerate, customPosition, transform.rotation);
     }
 }
