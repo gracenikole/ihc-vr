9335af46626317d8862d2be5b31574157678c4d4
Renato Rodriguez Llanos <renato.rodriguez.l@utec.edu.pe>
2023-07-10 08:43:38 -0500

    watering can missing

--- Assets/SpawnScript.cs
+++ Assets/SpawnScript.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class SpawnScript : MonoBehaviour
 {
     public GameObject cubePrefab;
+    public GameObject treePrefab;
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space)) {
-            Instantiate(cubePrefab, transform.position, Quaternion.identity);
+            //Instantiate(cubePrefab, transform.position, Quaternion.identity);
+            Instantiate(treePrefab, transform.position, Quaternion.identity);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        
     }
 }
